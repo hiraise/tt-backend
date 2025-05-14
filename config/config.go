0e78731f771e28ac0e19ad2d4562b4030c93733f
Ali Musaev <musaev.ae@modul.tech>
2025-05-14 17:31:44 +0300

    add tokens name in auth  config

--- config/config.go
+++ config/config.go
@@ -5,29 +5,31 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type App struct {
+type AppConfig struct {
 	Debug    bool   `env:"APP_DEBUG,required"`
 	RootPath string `env:"APP_ROOT_PATH,required"`
 }
 
-type PG struct {
+type PGConfig struct {
 	ConnString       string `env:"PG_CONNECTION_STRING,required"`
 	MaxPoolSize      int    `env:"PG_MAX_POOL_SIZE,required"`
 	MigrationEnabled bool   `env:"PG_MIGRATION_ENABLED" envDefault:"false"`
 	MigrationPath    string `env:"PG_MIGRATION_PATH" envDefault:"file://../../migrations"`
 }
 
-type AUTH struct {
-	AccessTokenSecret       string `env:"AUTH_ACCESS_TOKEN_SECRET,required"`
-	AccessTokenLifetimeMin  int    `env:"AUTH_ACCESS_TOKEN_LIFETIME_MIN,required"`
-	RefreshTokenSecret      string `env:"AUTH_REFRESH_TOKEN_SECRET,required"`
-	RefreshTokenLifetimeMin int    `env:"AUTH_REFRESH_TOKEN_LIFETIME_MIN,required"`
-	TokenIssuer             string `env:"AUTH_TOKEN_ISSUER,required"`
+type AuthConfig struct {
+	ATSecret    string `env:"AUTH_ACCESS_TOKEN_SECRET,required"`
+	ATLifeMin   int    `env:"AUTH_ACCESS_TOKEN_LIFETIME_MIN,required"`
+	ATName      string `env:"AUTH_ACCESS_TOKEN_NAME" envDefault:"at"`
+	RTSecret    string `env:"AUTH_REFRESH_TOKEN_SECRET,required"`
+	RTLifeMin   int    `env:"AUTH_REFRESH_TOKEN_LIFETIME_MIN,required"`
+	RTName      string `env:"AUTH_REFRESH_TOKEN_NAME" envDefault:"rt"`
+	TokenIssuer string `env:"AUTH_TOKEN_ISSUER,required"`
 }
 type Config struct {
-	App  App
-	PG   PG
-	AUTH AUTH
+	App  AppConfig
+	PG   PGConfig
+	AUTH AuthConfig
 }
 
 func New() (*Config, error) {
