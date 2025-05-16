29765a7f21bde66b73ca53c7d767ae5c86831913
Ali Musaev <musaev.ae@modul.tech>
2025-05-16 17:46:47 +0300

    add reqId and userId to log

--- internal/controller/http/middleware/recovery.go
+++ internal/controller/http/middleware/recovery.go
@@ -1,31 +1,37 @@
 package middleware
 
 import (
-	"log/slog"
 	"runtime"
+	"task-trail/internal/pkg/contextmanager"
 	"task-trail/pkg/logger"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RecoveryWithLogger(l logger.Logger) gin.HandlerFunc {
+const sourceCodeOffset = 4
+
+// recover server after panic
+func NewRecovery(l logger.Logger, m contextmanager.Gin) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if r := recover(); r != nil {
 				// TODO: refactor
-				pc, file, line, _ := runtime.Caller(2)
+				pc, file, line, _ := runtime.Caller(sourceCodeOffset)
 				fn := runtime.FuncForPC(pc)
 				path := map[string]any{
 					"file":     file,
 					"function": fn.Name(),
 					"line":     line,
 				}
-				l.Error("panic recovered",
-					slog.Any("error", r),
-					slog.String("path", c.Request.URL.Path),
-					slog.String("method", c.Request.Method),
-					slog.String("client_ip", c.ClientIP()),
-					slog.Any("source", path),
+				l.Error(
+					"panic recovered",
+					"error", r,
+					"path", c.Request.URL.Path,
+					"method", c.Request.Method,
+					"client_ip", c.ClientIP(),
+					"source", path,
+					"userId", m.GetUserID(c),
+					"reqId", m.GetRequestID(c),
 				)
 				c.AbortWithStatus(500)
 			}
