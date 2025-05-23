6a00d8c3b71177d396c5442bdba5188c134fd898
Ali Musaev <musaev.ae@modul.tech>
2025-05-23 19:54:56 +0300

    add email token entity

--- internal/entity/token.go
+++ internal/entity/token.go
@@ -2,10 +2,26 @@ package entity
 
 import "time"
 
-type Token struct {
+type EmailTokenPurpose string
+
+const (
+	PurposeConfirmation EmailTokenPurpose = "confirm"
+	PurposeReset        EmailTokenPurpose = "reset"
+)
+
+type RefreshToken struct {
 	ID        string
 	UserId    int
 	CreatedAt time.Time
 	ExpiredAt time.Time
 	RevokedAt *time.Time
+}
+
+type EmailToken struct {
+	ID        string
+	UserId    int
+	Purpose   EmailTokenPurpose
+	CreatedAt time.Time
+	ExpiredAt time.Time
+	UsedAt    *time.Time
 }
