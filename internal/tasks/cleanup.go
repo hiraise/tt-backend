3133fa595a3a3a701e2a7dd01db49231dd67df24
Ali Musaev <musaev.ae@modul.tech>
2025-05-30 11:34:43 +0300

    implement email token cleanup functionality and add corresponding tests

--- internal/tasks/cleanup.go
+++ internal/tasks/cleanup.go
@@ -9,20 +9,37 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func CleanupTokens(r repo.RefreshTokenRepository, l logger.Logger) {
-	c := cron.New()
-	_, err := c.AddFunc("0 3 * * *", func() {
+func CleanupRefreshTokens(r repo.RefreshTokenRepository, l logger.Logger) {
+	startNewTask("0 3 * * *", l, "cleanup refresh tokens", func() {
 		deleted, err := r.DeleteRevokedAndOldTokens(context.Background(), 7)
 		if err != nil {
-			l.Error("Failed to delete old and revoked tokens", "error", err)
+			l.Error("failed to delete old and revoked refresh tokens", "error", err)
 			return
 		}
-		l.Info("Complete delete old and revoked tokens", "deleted_tokens", deleted)
+		l.Info("complete delete old and revoked refresh tokens", "deleted_tokens", deleted)
 
 	})
+}
+
+func CleanupEmailTokens(r repo.EmailTokenRepository, l logger.Logger) {
+	startNewTask("30 3 * * *", l, "cleanup email tokens", func() {
+		deleted, err := r.DeleteUsedAndOldTokens(context.Background(), 7)
+		if err != nil {
+			l.Error("failed to delete old and used email tokens", "error", err)
+			return
+		}
+		l.Info("complete delete old and used email tokens", "deleted_tokens", deleted)
+
+	})
+}
+
+func startNewTask(spec string, l logger.Logger, name string, f func()) {
+	c := cron.New()
+	_, err := c.AddFunc(spec, f)
 	if err != nil {
 		l.Error("cron task start failed", "error", err.Error())
 		os.Exit(1)
 	}
 	c.Start()
+	l.Info("cron task successfully started", "task name", name, "spec", spec)
 }
