c50a9fe143f2535ea68a78a64dcaf56305174fed
Ali Musaev <musaev.ae@modul.tech>
2025-06-25 18:53:12 +0300

    add file usecase implementation

---
+++ internal/usecase/file/file.go
@@ -0,0 +1,68 @@
+package file
+
+import (
+	"context"
+	"errors"
+	"task-trail/internal/customerrors"
+	"task-trail/internal/entity"
+	"task-trail/internal/pkg/storage"
+	"task-trail/internal/pkg/uuid"
+	"task-trail/internal/repo"
+)
+
+type UseCase struct {
+	txManager  repo.TxManager
+	fileRepo   repo.FileRepository
+	storage    storage.Service
+	errHandler customerrors.ErrorHandler
+	uuidGen    uuid.Generator
+}
+
+func New(
+	txManager repo.TxManager,
+	fileRepo repo.FileRepository,
+	storage storage.Service,
+	errHandler customerrors.ErrorHandler,
+	uuidGen uuid.Generator,
+) *UseCase {
+	return &UseCase{
+		txManager:  txManager,
+		fileRepo:   fileRepo,
+		storage:    storage,
+		errHandler: errHandler,
+		uuidGen:    uuidGen,
+	}
+}
+
+func (u *UseCase) Save(
+	ctx context.Context,
+	ownerID int,
+	file []byte,
+	filename string,
+	mimeType string,
+) (string, error) {
+	name := u.uuidGen.Generate()
+	// register in db
+	f := &entity.File{
+		ID:           name,
+		OriginalName: filename,
+		MimeType:     mimeType,
+		OwnerID:      ownerID,
+	}
+	err := u.fileRepo.Create(ctx, f)
+	if err != nil {
+		if errors.Is(err, repo.ErrConflict) {
+			return "", u.errHandler.Conflict(err, "file already exists")
+		}
+		if errors.Is(err, repo.ErrNotFound) {
+			return "", u.errHandler.NotFound(err, "owner not found")
+		}
+		return "", u.errHandler.InternalTrouble(err, "failed to upload file")
+	}
+	// save in storage
+	if err := u.storage.Save(ctx, file, name, mimeType); err != nil {
+		return "", u.errHandler.InternalTrouble(err, "file storing failure")
+	}
+	return name, nil
+
+}
