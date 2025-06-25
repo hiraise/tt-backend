de12741bdbae45a351dd8656314ca651055fd6a5
Ali Musaev <musaev.ae@modul.tech>
2025-06-25 18:50:59 +0300

    add create file method

---
+++ internal/repo/persistent/file.go
@@ -0,0 +1,28 @@
+package persistent
+
+import (
+	"context"
+	"task-trail/internal/entity"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PgFileRepository struct {
+	PgRepostitory
+}
+
+func NewFileRepo(db *pgxpool.Pool) *PgFileRepository {
+	return &PgFileRepository{PgRepostitory{pg: db}}
+}
+
+func (r *PgFileRepository) Create(ctx context.Context, file *entity.File) error {
+	query := `
+		INSERT INTO files 
+		(id, original_name, mime_type, owner_id) 
+		VALUES ($1, $2, $3, $4)`
+	_, err := r.getDb(ctx).Exec(ctx, query, file.ID, file.OriginalName, file.MimeType, file.OwnerID)
+	if err != nil {
+		return r.handleError(err)
+	}
+	return nil
+}
