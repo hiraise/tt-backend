142b8f11d775533bd7c50533d65052df1bcc5744
Ali Musaev <musaev.ae@modul.tech>
2025-06-25 18:53:27 +0300

    add user usecase method implementations

--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -1,16 +1,100 @@
 package user
 
 import (
+	"context"
+	"errors"
+	"task-trail/internal/customerrors"
+	"task-trail/internal/entity"
 	"task-trail/internal/pkg/password"
+	"task-trail/internal/pkg/uuid"
 	"task-trail/internal/repo"
+	"task-trail/internal/usecase/file"
 )
 
 type UseCase struct {
-	txManager  repo.TxManager
-	repo       repo.UserRepository
-	pwdService password.Service
+	txManager   repo.TxManager
+	userRepo    repo.UserRepository
+	fileUseCase *file.UseCase
+	pwdService  password.Service
+	errHandler  customerrors.ErrorHandler
+	uuidGen     uuid.Generator
 }
 
-func New(txManager repo.TxManager, repo repo.UserRepository, pwdService password.Service) *UseCase {
-	return &UseCase{txManager: txManager, repo: repo, pwdService: pwdService}
+func New(
+	txManager repo.TxManager,
+	repo repo.UserRepository,
+	fileUseCase *file.UseCase,
+	pwdService password.Service,
+	errHandler customerrors.ErrorHandler,
+	uuidGen uuid.Generator,
+) *UseCase {
+	return &UseCase{
+		txManager:   txManager,
+		userRepo:    repo,
+		fileUseCase: fileUseCase,
+		pwdService:  pwdService,
+		errHandler:  errHandler,
+		uuidGen:     uuidGen,
+	}
+}
+
+func (u *UseCase) UpdateAvatar(
+	ctx context.Context,
+	userID int,
+	file []byte,
+	filename string,
+	mimeType string,
+) (string, error) {
+	var avatarID string
+	var err error
+	fn := func(ctx context.Context) error {
+		avatarID, err = u.fileUseCase.Save(ctx, userID, file, filename, mimeType)
+		if err != nil {
+			return err
+		}
+		err = u.userRepo.Update(ctx, &entity.User{ID: userID, AvatarID: &avatarID})
+		if err != nil {
+
+			if errors.Is(err, repo.ErrNotFound) {
+				return u.errHandler.BadRequest(err, "user not found", "userID", userID)
+			}
+			return u.errHandler.InternalTrouble(err, "user update failed", "userID", userID)
+		}
+		return nil
+	}
+
+	if err := u.txManager.DoWithTx(ctx, fn); err != nil {
+		return "", err
+	}
+	return avatarID, nil
+}
+
+func (u *UseCase) UpdateByID(ctx context.Context, data *entity.User) (*entity.User, error) {
+	err := u.userRepo.Update(ctx, data)
+	if err != nil {
+
+		if errors.Is(err, repo.ErrNotFound) {
+			return nil, u.errHandler.BadRequest(err, "user not found", "userID", data.ID)
+		}
+		return nil, u.errHandler.InternalTrouble(err, "user update failed", "userID", data.ID)
+	}
+	user, err := u.userRepo.GetByID(ctx, data.ID)
+	if err != nil {
+		if errors.Is(err, repo.ErrNotFound) {
+			return nil, u.errHandler.BadRequest(err, "user not found", "userID", data.ID)
+		}
+		return nil, u.errHandler.InternalTrouble(err, "user update failed", "userID", data.ID)
+	}
+	return user, nil
+}
+func (u *UseCase) GetByID(ctx context.Context, ID int) (*entity.User, error) {
+	user, err := u.userRepo.GetByID(ctx, ID)
+	if err != nil {
+		if errors.Is(err, repo.ErrNotFound) {
+			return nil, u.errHandler.BadRequest(err, "user not found", "userID", ID)
+		}
+		return nil, u.errHandler.InternalTrouble(err, "user update failed", "userID", ID)
+	}
+	return user, nil
+
 }
