d504f268617be1f211e1c3c4276237e3e3e35783
Ali Musaev <musaev.ae@modul.tech>
2026-05-10 05:12:31 +0300

    handle RT not found error

--- internal/application/service/access/logout.go
+++ internal/application/service/access/logout.go
@@ -23,25 +23,33 @@ func (s *LogoutService) Execute(ctx context.Context, rt string) error {
 	}
 	rtID := entity.RefreshTokenID(r)
 	userID := entity.UserID(u)
-	oldToken, err := s.refreshTokenRepository.GetUserToken(ctx, rtID, userID)
-	if err != nil {
-		// возможно токен может быть удален из бд так как старый, но нужно ли это считать 401 или 500 хз.
-		// по идее если токен разобрался но его в бд нет то можно просто 500 кинуть
-		return err
-	}
-	if err := oldToken.Validate(); err != nil {
-		var e *domain.DomainError
-		if errors.As(err, &e) {
-			if e.Code == domain.RefreshTokenAlreadyUsed {
-				// Revoke all
+	f := func(ctx context.Context) error {
+		oldToken, err := s.refreshTokenRepository.GetUserToken(ctx, rtID, userID)
+		if err != nil {
+			var e *domain.DomainError
+			if errors.As(err, &e) {
+				if e.Code == domain.EntityNotFound {
+					return domain.ErrRefreshTokenNotFound()
+				}
 			}
+			return err
 		}
-		return err
-	}
+		if err := oldToken.Validate(); err != nil {
+			var e *domain.DomainError
+			if errors.As(err, &e) {
+				if e.Code == domain.RefreshTokenAlreadyUsed {
+					// Revoke all
+				}
+			}
+			return err
+		}
 
-	oldToken.Use()
-	if err := s.refreshTokenRepository.Update(ctx, oldToken); err != nil {
-		return err
+		oldToken.Use()
+		if err := s.refreshTokenRepository.Update(ctx, oldToken); err != nil {
+			return err
+		}
+		return nil
 	}
-	return nil
+	return s.txManager.DoWithTx(ctx, f)
+
 }
